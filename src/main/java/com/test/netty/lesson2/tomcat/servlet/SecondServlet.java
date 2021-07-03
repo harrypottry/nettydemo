113cce93846c1d4df34126612822ca5bd5c50426
harrypotter12 <zjy_dhls1993@163.com>
2021-07-03 15:02:57 +0800

    fix name

--- src/main/java/com/test/netty/lesson2/tomcat/servlet/SecondServlet.java
+++ src/main/java/com/test/netty/lesson2/tomcat/servlet/SecondServlet.java
@@ -1,17 +1,17 @@
 package com.test.netty.lesson2.tomcat.servlet;
 
 
-import com.test.netty.lesson2.tomcat.http.GPRequest;
-import com.test.netty.lesson2.tomcat.http.GPResponse;
-import com.test.netty.lesson2.tomcat.http.GPServlet;
+import com.test.netty.lesson2.tomcat.http.MyRequest;
+import com.test.netty.lesson2.tomcat.http.MyResponse;
+import com.test.netty.lesson2.tomcat.http.MyServlet;
 
-public class SecondServlet extends GPServlet {
+public class SecondServlet extends MyServlet {
 
-	public void doGet(GPRequest request, GPResponse response) throws Exception {
+	public void doGet(MyRequest request, MyResponse response) throws Exception {
 		this.doPost(request, response);
 	}
 
-	public void doPost(GPRequest request, GPResponse response) throws Exception {
+	public void doPost(MyRequest request, MyResponse response) throws Exception {
 		response.write("This is Second Serlvet");
 	}
 
